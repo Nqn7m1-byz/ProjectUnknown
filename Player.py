2e8926331d81aeac1b9e447c071940449d260d06
Edwin <edwin.lry@gmail.com>
2022-08-22 10:50:16 +0800

    添加了移动

--- Player.py
+++ Player.py
@@ -1,37 +1,56 @@
-from enum import Enum
 import pygame
 
+
 class Player:
-    health = 0
-    attack = 0
-    intelligence = 0
-    phyDef = 0
-    magDef = 0
     level = 1
     exp = 0
     god = False
     maxHealth = 0
     inventory = list()
     magic = list()
-    x=100
-    y=30
-    facing=True
-    upper_left = (100, 30)
+    facing = True
     length_height = (210, 500)
-    transform = pygame.Rect(upper_left, length_height)
+    transform = None
 
-    def __init__(self, health, attack, phyDef, magDef):
+    def __init__(self, health, attack, phyDef, magDef, intelligence):
         self.health = health
         self.maxHealth = health
         self.attack = attack
         self.phyDef = phyDef
         self.magDef = magDef
+        self.intelligence = intelligence
+        self.image = pygame.image.load('player.png').convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.midbottom = (100, 130)
+        self.vel_y = 0
+        self.jumped = False
 
-    def Attack(self,monsters):
+    def Update(self):
+        x_move = 0
+        y_move = 0
+
+        key = pygame.key.get_pressed()
+        if key[pygame.K_SPACE] and self.jumped is False:
+            self.vel_y = -18
+            self.jumped = True
+        if not key[pygame.K_SPACE]:
+            self.jumped = False
+        if key[pygame.K_a]:
+            x_move -= 5
+        if key[pygame.K_d]:
+            x_move += 5
+        self.vel_y += 1.2
+        if self.vel_y > 10:
+            self.vel_y = 10
+        y_move += self.vel_y
+        self.rect.x += x_move
+        self.rect.y += y_move
+
+    def Attack(self, monsters):
         # put animation here
 
         for monster in monsters:
-            if()
+            pass
 
     def Magic(self):
         # put animation here
@@ -66,17 +85,6 @@ class Player:
             self.maxHealth += 10
             self.health = self.maxHealth
 
-    def Left(self):
-        self.x-=2
-
-    def Up(self):
-        self.y-=2
-
-    def Right(self):
-        self.x+=2
-
-    def Down(self):
-        pass
-
-    def Fall(self):
-        self.y-=3
+    def GodMode(self):
+        self.health=1000000000
+        self.maxHealth=self.health
--- main.py
+++ main.py
@@ -1,30 +1,27 @@
 import pygame
 from Player import Player
 
-running = True
 player = Player(10, 5, 5, 6)
+
 
 def main():
     pygame.init()
-    pygame.display.set_mode(1980,1080)
+    pygame.display.set_mode((1000, 500))
     pygame.display.set_caption("Untitled")
+    running = True
     while running:
-        CheckEvents()
+        running = CheckEvents()
+        player.update()
 
 
 def CheckEvents():
+    quit = True
     # input detection
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a:
-                player.Left()
-            if event.key == pygame.K_s:
-                player.Down()
-            if event.key == pygame.K_d:
-                player.Right()
-            if event.key == pygame.K_w:
-                player.Up()
-            if event.key == pygame.K_SPACE:
-                pass
+            quit = False
+    return quit
+
+
+if __name__ == '__main__':
+    main()
