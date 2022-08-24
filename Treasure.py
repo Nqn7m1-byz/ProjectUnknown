6d91e4be512ea51f4f63a18ef57485e65c512d09
FF-Yan <89203882+FF-Yan@users.noreply.github.com>
2022-08-24 16:22:25 +0800

    Update Treasure.py
    Signed-off-by: FF-Yan <89203882+FF-Yan@users.noreply.github.com>

--- Treasure.py
+++ Treasure.py
@@ -1,14 +1,16 @@
-import os
 import pygame
+import tools
 
 class Treasure(pygame.sprite.Sprite):
     def __init__(self, x, y, list):
         pygame.sprite.Sprite.__init__(self)
         self.x = x
         self.y = y
+
         good_treasure_rect = [(6, 9, 20, 19), (6, 36, 20, 24)]
         luxurious_treasure_rect = [(70, 137, 20, 19), (70, 164, 20, 24)]
         list = ["A", "B", "C"]
+
         if a:
             self.frame_rects = good_treasure_rect
         else:
@@ -16,30 +18,30 @@ class Treasure(pygame.sprite.Sprite):
 
         self.frames = []
         for frame_rect in self.frame_rects:
-            self.frames.append(get_image(load_graphics('Graphic')['treasure chests']), frame_rect)
+            self.frames.append(tools.get_image(tools.load_graphics('Graphic')['treasure chests']), frame_rect)
 
         self.frame_index = 0
         self.image = self.frames[self.frame_index]
         self.rect = self.image.get_rect()
         self.rect.x = self.x
         self.rect.y = self.y
+        self.state = 'rest'
 
-def load_graphics(path):
-    graphics = {}
-    for pic in os.listdir(path):
-        name, ext = os.path.splitext(pic)
-        img = pygame.image.load(os.path.join(path, pic))
-        if img.get_alpha():
-            img = img.convert_alpha()
-        else:
-            img = img.convert()
-        graphics[name] = img
-    return graphics
+    def update(self):
+        self.current_time = pygame.time.get_ticks()
+        self.handle_states()
 
-def get_image(sheet, x, y, width, height):
-    image = pygame.Surface((width, height))
-    image.blit(sheet, (0,0), (x, y, width, height))
-    image = pygame.transform.scale(image, (width, height))
-    return image
+    def handle_states(self):
+        if self.state == 'rest':
+            self.rest()
+        elif self.state == 'open':
+            self.open()
+
+    def rest(self):
+        pass
+
+    def open(self):
+        self.frame_index = 1
+        self.image = self.frames[self.frame_index]
 
 
